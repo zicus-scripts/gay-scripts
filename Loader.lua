bd2090b2bdc03140a3e97f6ec4a58add3e7e159b
zicus-scripts <132674327+zicus-scripts@users.noreply.github.com>
2023-07-02 14:55:23 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9061d1f62d398a63716c7fc98fd130ff.lua"))()
+if game.PlaceId == 13127800756 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/01b105207e6224d87a5b870bca0edbb4.lua"))()
+elseif game.PlaceId == 13230182363 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dd39d705a3721922aa908b746d8e4973.lua"))()
+else
+    game.Players.LocalPlayer:Kick("Invalid Game! View games @ https://discord.gg/NHykD5JAPq")
+end
